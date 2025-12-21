c883d05ffe779a3b310146fed02d9e62da9f1b48
sujal <thesujalogy@gmail.com>
2025-12-21 12:27:42 +0530

    add

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
     port: 8080,
     allowedHosts: ["school-directory.llf.org.in"]
   },
